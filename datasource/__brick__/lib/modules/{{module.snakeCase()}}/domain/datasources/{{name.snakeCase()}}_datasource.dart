aecd5562e60b94e4ca5522bf710c9202696ecd25
McQuenji <60017181+mcquenji@users.noreply.github.com>
2024-07-20 20:34:57 +0200

    fix: replace unnecessary double quotes with single quotes

--- datasource/__brick__/lib/modules/{{module.snakeCase()}}/domain/datasources/{{name.snakeCase()}}_datasource.dart
+++ datasource/__brick__/lib/modules/{{module.snakeCase()}}/domain/datasources/{{name.snakeCase()}}_datasource.dart
@@ -2,7 +2,7 @@ import 'package:mcquenji_core/mcquenji_core.dart';
 
 abstract class {{name.pascalCase()}}DataSource extends DataSource{
   @override
-  String get name => "{{name.pascalCase()}}";
+  String get name => '{{name.pascalCase()}}';
 
   @override
   void dispose() {}
--- service/__brick__/lib/modules/{{module.snakeCase()}}/domain/services/{{name.snakeCase()}}_service.dart
+++ service/__brick__/lib/modules/{{module.snakeCase()}}/domain/services/{{name.snakeCase()}}_service.dart
@@ -2,7 +2,7 @@ import 'package:mcquenji_core/mcquenji_core.dart';
 
 abstract class {{name.pascalCase()}}Service extends Service{
   @override
-  String get name => "{{name.pascalCase()}}";
+  String get name => '{{name.pascalCase()}}';
 
   @override
   void dispose() {}
