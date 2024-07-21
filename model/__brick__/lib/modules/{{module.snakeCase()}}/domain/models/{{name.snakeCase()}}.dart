9de8af1375e003b66cc1928ea0e4f5345c4b0874
McQuenji <60017181+mcquenji@users.noreply.github.com>
2024-07-22 01:17:00 +0200

    fix(brick/model): put unnamed constructor first

--- model/__brick__/lib/modules/{{module.snakeCase()}}/domain/models/{{name.snakeCase()}}.dart
+++ model/__brick__/lib/modules/{{module.snakeCase()}}/domain/models/{{name.snakeCase()}}.dart
@@ -6,9 +6,9 @@ part '{{name.snakeCase()}}.freezed.dart';
 
 @freezed
 class {{name.pascalCase()}} with _${{name.pascalCase()}} {
-  const {{name.pascalCase()}}._();
-
   const factory {{name.pascalCase()}}({}) = _{{name.pascalCase()}};
+
+  const {{name.pascalCase()}}._();
 
   {{^skipSerialization}}factory {{name.pascalCase()}}.fromJson(Map<String, Object?> json) => _${{name.pascalCase()}}FromJson(json);{{/skipSerialization}}
 }
