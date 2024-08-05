a4a0a7eadde490570c7282bc9de2b1053d0b3519
McQuenji <60017181+mcquenji@users.noreply.github.com>
2024-08-05 17:41:24 +0200

    fix(brick/module): use debugLogHandler instead of print

--- module/__brick__/test/{{name.snakeCase()}}_test.dart
+++ module/__brick__/test/{{name.snakeCase()}}_test.dart
@@ -1,13 +1,12 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:{{package}}/modules/{{name.snakeCase()}}/{{name.snakeCase()}}.dart';
 import 'package:logging/logging.dart';
+import 'package:mcquenji_core/mcquenji_core.dart';
 
 Future<void> main() async {
   Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen(print);
+  Logger.root.onRecord.listen(debugLogHandler);
 
   setUp(() {
     Modular.init({{name.pascalCase()}}Module());
