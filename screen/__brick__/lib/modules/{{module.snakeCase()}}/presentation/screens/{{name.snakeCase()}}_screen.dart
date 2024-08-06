7a61889136e3e90e7af7f2500ab5b8722c4846a7
McQuenji <60017181+mcquenji@users.noreply.github.com>
2024-08-06 22:31:27 +0200

    fix: append screen suffix to constructor

--- screen/__brick__/lib/modules/{{module.snakeCase()}}/presentation/screens/{{name.snakeCase()}}_screen.dart
+++ screen/__brick__/lib/modules/{{module.snakeCase()}}/presentation/screens/{{name.snakeCase()}}_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:{{package}}/modules/{{module.snakeCase()}}/{{module.snakeCase()}}.dart';
 
 class {{name.pascalCase()}}Screen extends StatelessWidget {
-  const {{name.pascalCase()}}({super.key});
+  const {{name.pascalCase()}}Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
