24aedd1a3e00c6c239c9c3986a321ec7a137aee1
McQuenji <60017181+mcquenji@users.noreply.github.com>
2024-08-17 19:47:58 +0200

    feat: add flutter_animate

--- screen/__brick__/lib/modules/{{module.snakeCase()}}/presentation/screens/{{name.snakeCase()}}_screen.dart
+++ screen/__brick__/lib/modules/{{module.snakeCase()}}/presentation/screens/{{name.snakeCase()}}_screen.dart
@@ -2,6 +2,8 @@ import 'package:mcquenji_core/mcquenji_core.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:{{package}}/modules/{{module.snakeCase()}}/{{module.snakeCase()}}.dart';
+import 'package:lb_planner/gen/assets/assets.gen.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class {{name.pascalCase()}}Screen extends StatelessWidget {
   const {{name.pascalCase()}}Screen({super.key});
--- widget/__brick__/lib/modules/{{module.snakeCase()}}/presentation/widgets/{{name.snakeCase()}}.dart
+++ widget/__brick__/lib/modules/{{module.snakeCase()}}/presentation/widgets/{{name.snakeCase()}}.dart
@@ -2,6 +2,9 @@ import 'package:mcquenji_core/mcquenji_core.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:{{package}}/modules/{{module.snakeCase()}}/{{module.snakeCase()}}.dart';
+import 'package:lb_planner/gen/assets/assets.gen.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
 
 class {{name.pascalCase()}} extends StatelessWidget {
   const {{name.pascalCase()}}({super.key});
