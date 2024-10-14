c7e13c304af773d5f86cedf8e50ea76cb7d6b91c
McQuenji <60017181+mcquenji@users.noreply.github.com>
2024-10-14 17:50:09 +0200

    fix: update handler args type

--- handler/__brick__/lib/modules/{{module.snakeCase()}}/presentation/handlers/{{name.snakeCase()}}_handler.dart
+++ handler/__brick__/lib/modules/{{module.snakeCase()}}/presentation/handlers/{{name.snakeCase()}}_handler.dart
@@ -3,6 +3,6 @@ import 'package:shelf_modular/shelf_modular.dart';
 import 'package:shelf/shelf.dart';
 
 
-Future<Response> {{name.camelCase()}}Handler(Request request, Injector i, ModularRouteArgs args) async {
+Future<Response> {{name.camelCase()}}Handler(Request request, Injector i, ModularArguments args) async {
   return Response.ok('Hello, World!');
 }
