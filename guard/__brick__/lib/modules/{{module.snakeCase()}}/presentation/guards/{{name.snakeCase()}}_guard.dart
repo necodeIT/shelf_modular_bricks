4e09ebb26773ac271cffaed6908f100a41f7d7da
mcquenji <benjamin.mceachnie@necode.it>
2024-10-16 08:44:39 +0000

    fix(brick/guard): use shelf_modular guard signature

--- guard/__brick__/lib/modules/{{module.snakeCase()}}/presentation/guards/{{name.snakeCase()}}_guard.dart
+++ guard/__brick__/lib/modules/{{module.snakeCase()}}/presentation/guards/{{name.snakeCase()}}_guard.dart
@@ -1,11 +1,14 @@
+import 'dart:async';
+
 import 'package:mcquenji_core/mcquenji_core.dart';
 import 'package:shelf_modular/shelf_modular.dart';
+import 'package:shelf/shelf.dart';
 import 'package:{{package}}/modules/{{module.snakeCase()}}/{{module.snakeCase()}}.dart';
 
 
 class {{name.pascalCase()}}Guard extends RouteGuard{
   @override
-  Future<bool> canActivate(String path, ModularRoute route) async {
+  FutureOr<bool> canActivate(Request request, Route route) async {
     return true;
   }
 }
